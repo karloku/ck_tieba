21dce5cc434dcf58fcd60a3bc510f3fdf88f572f
Karloku Sang <karloku@gmail.com>
2015-05-23 20:54:33 +0800

    use flat ui

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,30 @@
-angular.module('app', ['components'])
- 
-.controller('Members', function ($scope, $http) {
+angular.module('app', ['ui.router','ui.bootstrap','ui.bootstrap.tpls'])
+.config(function($stateProvider, $urlRouterProvider){
+      
+  // For any unmatched url, send to /route1
+  $urlRouterProvider.otherwise("/members");
+
+  $stateProvider
+    .state('MembersView', {
+      url: "/members",
+      templateUrl: "members.html",
+      controller: 'membersController'
+    })
+    .state('HighlightView', {
+      url: "/highlights",
+      templateUrl: "highlights.html",
+      controller: 'highlightsController'
+    });
+})
+.controller('indexController', ['$scope','$state',function($scope, $state){
+    
+}])
+.controller('membersController', function ($scope, $http) {
   $http.get('api/members').success(function(data) {
     $scope.members = data;
   });
 })
-.controller('Highlights', function ($scope, $http) {
+.controller('highlightsController', function ($scope, $http) {
   $http.get('api/highlights').success(function(data) {
     $scope.highlights = data;
   });
